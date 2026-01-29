66230cc24516019555cf5ee8cde5c3cbbdc2034b
Samuel Prigent <samuel.prigent@yahoo.fr>
2026-01-29 22:53:48 +0100

    Fix oAuth

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         source: '/(.*)',
         headers: [
           { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           {
